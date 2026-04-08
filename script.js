91ae9b34df21c10c15e612793ccdfaeb156ee950
austin100-v <austinirungu13@gmail.com>
2026-04-08 11:07:15 +0300

    used classes instead of a constructor

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ const myLibrary = [];
 
 
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = crypto.randomUUID();
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.id = crypto.randomUUID();
+// }
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = crypto.randomUUID();
+    }
 }
 
 
